baec5800737e08cb85f89da259d6b88bf66c7462
Luan <luan@plannerti.com.br>
2021-05-30 13:29:28 -0300

    aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5;
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60){ //Condição simples!s
+    console.log('Você ultrapassou a velocidade permitda. MULTADO!')
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
